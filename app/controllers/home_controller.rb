cf912b25a994a522687fe76e9f8f92cd6d571702
James Agawa <james.agawa@gmail.com>
2014-12-02 17:33:08 +0800

    Added Home screen

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,68 @@
+class HomeViewController < UIViewController
+
+  def viewDidLoad
+    super
+
+    view.backgroundColor = UIColor.whiteColor
+    view.userInteractionEnabled = true
+    @button = showSearchButton
+    @label = showResultLabel
+  end
+
+  def searchStore
+    animateShowAnswer "Restaurant name goes here..."
+  end
+
+  def animateShowAnswer(answer)
+    UIView.animateWithDuration(1.0,
+      animations:lambda {
+        @label.alpha = 0
+        @label.transform = CGAffineTransformMakeScale(0.1, 0.1)
+      },
+      completion:lambda { |finished|
+        @label.text = answer
+        UIView.animateWithDuration(1.0,
+          animations:lambda {
+            @label.alpha = 1
+            @label.transform = CGAffineTransformIdentity
+          }
+        )
+      }
+    )
+  end
+
+  def showSearchButton
+    button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    button.center = CGPointZero
+    button.setTitle("Saan Tayo Kakain?", forState: UIControlStateNormal)
+    button.backgroundColor = UIColor.redColor
+    button.setTitleColor(UIColor.whiteColor, forState: UIControlStateNormal)
+    button.frame = [
+      [10, 75],
+      [screen_width - 20, 50]
+    ]
+    button.addTarget(self,
+                 action: 'searchStore',
+                 forControlEvents: UIControlEventTouchUpInside)
+    view.addSubview button
+    button
+  end
+
+  def showResultLabel
+    label = UILabel.alloc.initWithFrame([[10, 150], [screen_width - 20, 50]])
+    label.backgroundColor = UIColor.clearColor
+    label.text = ""
+    label.font = UIFont.boldSystemFontOfSize(15)
+    label.textColor = UIColor.redColor
+    label.textAlignment = UITextAlignmentCenter
+
+    view.addSubview label
+    label
+  end
+
+  def screen_width
+    view.frame.size.width
+  end
+
+end
+
